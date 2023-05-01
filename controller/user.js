62aa7d65bcd2b73483062db7faec1a834cab755e
deepak-420 <83333080+deepak-420@users.noreply.github.com>
2023-05-01 15:20:10 +0530

    initial commit

---
+++ controller/user.js
@@ -0,0 +1,72 @@
+import { User } from "../models/user.js";
+import bcrypt from 'bcrypt';
+import { sendCookie } from "../utils/features.js";
+import ErrorHandler from "../middlewares/error.js";
+
+export const register = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        let user = await User.findOne({ email })
+
+        if (user) return next(new ErrorHandler("User already exist", 404))
+
+        // if (user) return res.status(404).json({
+        //     success: false,
+        //     message: "User already exist"
+        // })
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        user = await User.create({ name, email, password: hashedPassword })
+
+        sendCookie(user, res, "Registered Successfully", 201)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email }).select("+password")
+
+        if (!user) return next(new ErrorHandler("Invalid email or password", 404))
+
+        // if (!user) return res.status(404).json({
+        //     success: false,
+        //     message: "Invalid email or password"
+        // })
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) return next(new ErrorHandler("Invalid email or password", 404))
+
+        // if (!isMatch) return res.status(404).json({
+        //     success: false,
+        //     message: "Invalid email or password"
+        // })
+
+        sendCookie(user, res, `Welcome back, ${user.name}`, 200)
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+export const getMyProfile = (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        user: req.user
+    })
+}
+
+export const logout = (req, res) => {
+    res.status(200).cookie("token", "", { expires: new Date(Date.now()) }).json({
+        success: true,
+        message: "Logged Out Successfully",
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true
+    })
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+import { app } from "./app.js";
+import { connectDB } from "./data/database.js";
+
+connectDB();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listenting at port ${process.env.PORT} on ${process.env.NODE_ENV} mode`)
+})
